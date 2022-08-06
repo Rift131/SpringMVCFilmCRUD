2ec0aab8ceffbd7246850426c635a9a7e929428f
Austin Borck <austinborck98@gmail.com>
2022-08-05 18:37:41 -0600

    on a server

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,8 +1,19 @@
 package com.skilldistillery.film.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FilmController {
+	@RequestMapping("route.do")
+	 public ModelAndView processData(@RequestParam("data") String s) {
+	   String allCaps = s.toUpperCase();
+	   ModelAndView mv = new ModelAndView();
+	   mv.setViewName("view.jsp");
+	   mv.addObject("result", allCaps);
+	   return mv;
+	 }
 
 }
